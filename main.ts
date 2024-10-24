4ab9afb206d2e321fe4c0b67f56fc67436648a04
hirokazu-ando <h.ando.taich@gmail.com>
2024-10-24 16:20:56 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
 basic.forever(function () {
-    if (Plumbot.isLeftLineSensorWhite() && Plumbot.isRightLineSensorBlack()) {
-        Plumbot.turnRight()
-    } else if (Plumbot.isLeftLineSensorBlack() && Plumbot.isRightLineSensorWhite()) {
-        Plumbot.turnLeft()
-    } else {
-        Plumbot.moveForward()
-    }
+	
 })
